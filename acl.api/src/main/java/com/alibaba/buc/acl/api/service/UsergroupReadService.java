e7c0f5b196161d56d9743b6eec8faac99e55570b
geekchen.zc <geekchen.zc@alibaba-inc.com>
2017-10-23 17:27:48 +0800

    usergroup search

--- acl.api/src/main/java/com/alibaba/buc/acl/api/service/UsergroupReadService.java
+++ acl.api/src/main/java/com/alibaba/buc/acl/api/service/UsergroupReadService.java
@@ -1,9 +1,14 @@
 package com.alibaba.buc.acl.api.service;
 
+import com.alibaba.buc.acl.api.common.AclResult;
 import com.alibaba.buc.acl.api.input.usergroup.GetUsergroupParam;
 import com.alibaba.buc.acl.api.input.usergroup.GetUsergroupUsersParam;
+import com.alibaba.buc.acl.api.input.usergroup.SearchUsergroupParam;
 import com.alibaba.buc.acl.api.output.usergroup.UsergroupResult;
 import com.alibaba.buc.acl.api.output.usergroup.UsergroupUsersResult;
+import com.alibaba.buc.api.common.AclPageResult;
+import com.alibaba.buc.api.common.AclPagination;
+import com.alibaba.buc.api.datapermission.result.DataPermissionModelResult;
 
 /**
  * Usergroup Read Service
@@ -13,4 +18,6 @@ public interface UsergroupReadService {
     UsergroupResult getUsergroup(GetUsergroupParam getUsergroupParam);
 
     UsergroupUsersResult getUsergroupUsers(GetUsergroupUsersParam getUsergroupUsersParam);
+
+    AclResult<AclPageResult<UsergroupResult>> searchUsergroup(SearchUsergroupParam searchUsergroupParam, AclPagination page);
 }
